35d8107520abc7d0b8048e88661ef9f65fc0bd19
Arun Ganesan <aganesan94@gmail.com>
2016-07-06 23:59:56 -0400

    Added update related use cases.

---
+++ amazondynamodbexperiments/src/main/java/org/aganesan/amazondynamodbexperiments/dataplane/UpdateItem.java
@@ -0,0 +1,93 @@
+package org.aganesan.amazondynamodbexperiments.dataplane;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+
+/**
+ * Demonstrates update items into dynamodb
+ * 
+ * @author agane
+ *
+ *
+ *         If the item does not exist, create it if the item exists, update it.
+ *         It is also possible to update the item with the putItem method though
+ *         it is not recommended. The difference between updating via putItem
+ *         and updateItem method is that the put Item method removes all the
+ *         attributes which are not specified in the request.
+ */
+public class UpdateItem extends BaseItem {
+
+	public UpdateItem() {
+		super();
+	}
+
+	@Test
+	// note that the InsertItem.case1 will need to be run prior to executing
+	// this statement.
+	public void case1() {
+
+		// Goal
+		// 1. Update first Name to Mary
+		// 2. Remove attribute colorPencils
+		// 3. Add attribute books. (Note it 
+
+		try {
+			Table table = dynamoDB.getTable(tableName);
+
+			Map<String, String> expressionAttributeNames = new HashMap<String, String>();
+			expressionAttributeNames.put("#fName", "firstName");
+			expressionAttributeNames.put("#colorPencils", "colorPencils");
+			expressionAttributeNames.put("#books", "books");
+			
+			List<String> books = Arrays.asList("Alice in wonderland", "Famous Five", "Autobiography of a Yogi");
+			
+			Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+			expressionAttributeValues.put(":val1", "Mary");
+			expressionAttributeValues.put(":val2", new HashSet<String>(books)); // Price
+			
+			UpdateItemOutcome outcome = table.updateItem("personId", // key
+																		// attribute
+																		// name
+					1, // key attribute value
+					"set #fName = :val1 add #books :val2 remove #colorPencils", // UpdateExpression
+					expressionAttributeNames, expressionAttributeValues);
+		} catch (Exception e) {
+			printStackTrace(e);
+		}
+	}
+
+	@Test
+	public void case2() {
+
+		try {
+			Table table = dynamoDB.getTable(tableName);
+
+			Map<String, String> expressionAttributeNames = new HashMap<String, String>();
+			expressionAttributeNames.put("#fName", "firstName");
+			expressionAttributeNames.put("#colorPencils", "colorPencils");
+			expressionAttributeNames.put("#books", "books");
+			
+			Map<String, Object> expressionAttributeValues = new HashMap<String, Object>();
+			expressionAttributeValues.put(":val1", "Gary");
+			expressionAttributeValues.put(":val2",new HashSet<String>(Arrays.asList("Red", "Green", "Blue"))); // Price
+
+			UpdateItemOutcome outcome = table.updateItem("personId", // key
+																		// attribute
+																		// name
+					1, // key attribute value
+					"set #fName = :val1 remove #books add #colorPencils :val2", // UpdateExpression
+					expressionAttributeNames, expressionAttributeValues);
+		} catch (Exception e) {
+			printStackTrace(e);
+		}
+	}
+	
+}
